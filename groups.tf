07608d3853f4fe2e269ca4c3bbb25dd793682e69
Cristian Beraha <cristian@cloudops.works>
2024-09-13 18:36:21 -0300

    chore: group configuration missed policy_refs!

--- groups.tf
+++ groups.tf
@@ -10,6 +10,7 @@ locals {
     if try(group.name, "") != "" && try(group.name_prefix, "") == ""
   }
 
+  # This is only for policy attachments
   named_policy_att = merge([
     for group in local.named_group_map : {
       for policy in try(group.policy_attachments, []) : "${group.name}-${policy}" => {
@@ -28,6 +29,25 @@ locals {
       for policy in try(group.policy_attachments, []) : "${group.name}-${policy}" => {
         group_name = group.name_prefix
         policy_arn = policy
+      }
+    }
+  ]...)
+
+
+  # this is only for policy refs, needs to check prefixed & named policies defined in the policy module
+  named_policy_refs_att = merge([
+    for group in local.named_group_map : {
+      for policy in try(group.policy_refs, []) : "${group.name}-${policy}" => {
+        group_name = group.name
+        policy_ref = policy
+      }
+    }
+  ]...)
+  prefixed_policy_refs_att = merge([
+    for group in local.group_map : {
+      for policy in try(group.policy_refs, []) : "${group.name}-${policy}" => {
+        group_prefix = group.name_prefix
+        policy_ref   = policy
       }
     }
   ]...)
@@ -69,4 +89,15 @@ resource "aws_iam_group_policy_attachment" "prefixed" {
   policy_arn = data.aws_iam_policy.prefixed[each.key].arn
 }
 
+resource "aws_iam_group_policy_attachment" "named_refs" {
+  for_each   = local.named_policy_refs_att
+  group      = aws_iam_group.named[each.value.group_name].name
+  policy_arn = try(aws_iam_policy.named[each.value.policy_ref].arn, aws_iam_policy.prefixed[each.value.policy_ref].arn)
+}
+
+resource "aws_iam_group_policy_attachment" "prefixed_refs" {
+  for_each   = local.prefixed_policy_refs_att
+  group      = aws_iam_group.prefixed[each.value.group_prefix].name
+  policy_arn = try(aws_iam_policy.named[each.value.policy_ref].arn, aws_iam_policy.prefixed[each.value.policy_ref].arn)
+}
 # TODO: Inline Policies
